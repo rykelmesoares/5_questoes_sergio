f9cf536ef0ba4f6c9ab5980d00550ba3e7611873
Kayki-Borges <cicero.oliveira139@aluno.ce.gov.br>
2023-10-10 11:09:16 -0300

    desafio
    choremmmm

---
+++ desafio.py
@@ -0,0 +1,6 @@
+altura=float(input('digite a altura'))
+largura=float(input('digite a largura'))
+area=altura*largura
+print('a área da sua parede é: ' ,area, 'm²')
+tinta=area/2
+print('a tinta necessária é: ', tinta, 'litros')
